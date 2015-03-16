573c6f9e27973b5074d81bf272f0351012551ccf
macg19-91 <macg19-91@hotmail.com>
2015-03-16 09:36:49 -0600

    primero

---
+++ Altel/src/utils/TextPrinter.java
@@ -0,0 +1,275 @@
+
+package utils;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.print.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jnlp.PrintService;
+import javax.print.DocPrintJob;
+import javax.print.PrintServiceLookup;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.PrintServiceAttributeSet;
+import javax.print.attribute.standard.PrinterName;
+
+/**
+* Utility class to print some lines of text to
+* the default printer.  Uses some default
+* font settings, and gets the page size from
+* the PrinterJob object.
+*
+* Note: this little example class does not
+* handle pagination.  All the text must fit
+* on a single page.
+* 
+* This class can also be used as a standalone
+* utility.  If the main method is invoked, it
+* reads lines of text from System.in, and
+* prints them to the default printer.
+*/
+
+public class TextPrinter implements Printable {
+
+                 static PrinterJob pjob = PrinterJob.getPrinterJob();
+    int cual=0;
+    boolean selected=false;
+    boolean caja=false;
+    String[] lineas;
+    public int print(Graphics g, PageFormat pf, int page) throws
+                                                        PrinterException {
+
+        if (page > 0) { /* We have only one page, and 'page' is zero-based */
+            return NO_SUCH_PAGE;
+        }
+
+        /* User (0,0) is typically outside the imageable area, so we must
+         * translate by the X and Y values in the PageFormat to avoid clipping
+         */
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.translate(pf.getImageableX(), pf.getImageableY());
+        int place=30;
+        /* Now we perform our rendering */
+        if(!caja){
+        BufferedReader br;
+        File Bitacora;
+       if(Static.isWindows()) Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+       else Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+        String[] lista=Bitacora.list();
+        try {
+            if(Static.isWindows()){
+                if(selected){
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+lista[cual]));
+                }else{
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+lista[Bitacora.list().length-3]));
+                }
+            }else{
+                if(selected){
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+lista[cual]));
+                }else{
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+lista[Bitacora.list().length-3]));
+                }
+            }
+            String line;
+        /*int cuenta=0;
+        try {
+        while ((line = br.readLine()) != null) {      
+                g.drawString(line, 0, place); 
+                place+=20;
+           cuenta++;
+        }*/
+        int cuenta=0;
+        String tipo="";
+        try {
+        while((line = br.readLine()) != null) {   
+            if(cuenta==0)g.drawString("Punto de venta: "+line, 0, place); 
+            if(cuenta==1)g.drawString("Fecha: "+line, 0, place); 
+            if(cuenta==2)g.drawString("Hora: "+line, 0, place); 
+            if(cuenta==3){tipo=line;place-=20;}
+            switch (tipo) {
+                case "Recargas":
+                    if(cuenta==4)g.drawString("Recarga automática (Tiempo Aire) Teléfono: "+line, 0, place); 
+                    if(cuenta==5)g.drawString("Transacción: "+line, 0, place); 
+                    if(cuenta==6)g.drawString("Monto: "+line, 0, place); 
+                break;
+                    
+                case "Pines":
+                    if(cuenta==4)g.drawString("Numero Pin: "+line, 0, place); 
+                    if(cuenta==5)g.drawString("Venta Pin: "+line, 0, place); 
+                    if(cuenta==6)g.drawString("Monto: "+line, 0, place); 
+                break;
+                    
+                case "Servicios":
+                    if(cuenta==4)g.drawString("Identificador: "+line, 0, place); 
+                    if(cuenta==5)g.drawString("Servicio: "+line, 0, place); 
+                    if(cuenta==6)g.drawString("Monto: "+line, 0, place); 
+                break;
+            }              
+           place+=20;      
+           cuenta++;
+        }
+        g.drawString("Gracias...", 0, place); 
+        br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }else{
+            for (String linea : lineas) {
+                g.drawString(linea, 0, place);
+                place+=20; 
+            }
+        }
+        /* tell the caller that this page is part of the printed document */
+        return PAGE_EXISTS;
+    }
+    public void setLineas(String[] lineas){
+        this.lineas=lineas;
+    }
+    private String getFecha() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        return  addCero(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)-1),2) +"/"+ addCero(Integer.toString(cal.get(Calendar.MONTH)+1),2) +"/"+ Integer.toString(cal.get(Calendar.YEAR));
+    }
+
+    private String getMes() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        return addCero(Integer.toString(cal.get(Calendar.MONTH)+1),2);
+    }
+    
+    private String addCero(String monto, int i) {
+        return String.format("%0"+i+"d", Integer.parseInt(monto));
+    };
+    
+    public String loadFileToArea(Boolean selected, int cual){
+        String reporte="";
+        BufferedReader br;
+         File Bitacora;
+       if(Static.isWindows()) Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+       else Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+        String[] lista=Bitacora.list();
+        try {
+            if(Static.isWindows()){
+                if(selected){
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+lista[cual]));
+                }else{
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+lista[Bitacora.list().length-3]));
+                }
+            }else{
+                if(selected){
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+lista[cual]));
+                }else{
+                    br = new BufferedReader(new FileReader(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+lista[Bitacora.list().length-3]));
+                }
+            }
+            String line;
+        int cuenta=0;
+        String tipo="";
+        try {
+        while((line = br.readLine()) != null) {   
+            if(cuenta==0)reporte+="Punto de venta: "+line+"\n";
+            if(cuenta==1)reporte+="Fecha: "+line+"\n";
+            if(cuenta==2)reporte+="Hora: "+line+"\n";
+            if(cuenta==3)tipo=line;
+            switch (tipo) {
+                case "Recargas":
+                    if(cuenta==4)reporte+="Recarga automática (Tiempo Aire) Teléfono: "+line+"\n";
+                    if(cuenta==5)reporte+="Transacción: "+line+", ";
+                    if(cuenta==6)reporte+="Monto: "+line+"\n";
+                break;
+                    
+                case "Pines":
+                    if(cuenta==4)reporte+="Numero Pin: "+line+"\n";
+                    if(cuenta==5)reporte+="Venta Pin: "+line+", ";
+                    if(cuenta==6)reporte+="Monto: "+line+"\n";
+                break;
+                    
+                case "Servicios":
+                    if(cuenta==4)reporte+="Identificador: "+line+"\n";
+                    if(cuenta==5)reporte+="Servicio: "+line+"\n";
+                    if(cuenta==6)reporte+="Monto: "+line+"\n";
+                break;
+            }
+                    
+           cuenta++;
+        }
+        reporte+="Gracias...";
+        br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return reporte;
+    }
+    public static void test() throws PrinterException {
+        DocPrintJob docPrintJob=null;
+        String printerNameDesired;
+        if(Static.isWindows())printerNameDesired = new auth().leerArchivo("Sesion\\defaultPrinter.txt");
+        else printerNameDesired = new auth().leerArchivo("Sesion/defaultPrinter.txt");
+        javax.print.PrintService[] service = PrinterJob.lookupPrintServices(); // list of printers
+
+    int count = service.length;
+
+    for (int i = 0; i < count; i++) {
+        if (service[i].getName().equalsIgnoreCase(printerNameDesired )) {
+            docPrintJob = service[i].createPrintJob();
+            i = count;
+        }
+    }
+//PrinterJob pjob = PrinterJob.getPrinterJob();
+pjob.setPrintService(docPrintJob.getPrintService());
+pjob.setJobName("job");
+//pjob.print();
+    }
+    public void actionPerformed(ActionEvent e) {
+         PrinterJob job = PrinterJob.getPrinterJob();
+         job.setPrintable(this);
+         boolean ok = job.printDialog();
+         if (ok) {
+             try {
+                  job.print();
+             } catch (PrinterException ex) {
+              /* The job did not successfully complete */
+             }
+         }
+    }
+
+    public boolean setPrinter() throws IOException { 
+                  //PrinterJob job = PrinterJob.getPrinterJob();
+         pjob.setPrintable(this);
+         boolean ok = pjob.printDialog();
+         
+         if (ok) {
+            new auth().escribeFichero(pjob.getPrintService().getName(),"Sesion/defaultPrinter.txt");
+            return true;
+         }
+         return false;
+    }
+    public void startPrinter(Boolean selected, int cual,Boolean caja) {
+ 
+                  //PrinterJob job = PrinterJob.getPrinterJob();
+         pjob.setPrintable(this);
+        /* boolean ok = job.printDialog();
+         if (ok) {
+             */
+            this.selected=selected;
+            this.cual=cual;
+            this.caja=caja;
+             try {
+                  pjob.print();
+             } catch (PrinterException ex) {
+              JOptionPane.showMessageDialog(null, "Revise la conexión a la impresora");
+             }
+        // }
+    }
+}
---
+++ Altel/src/utils/auth.java
@@ -0,0 +1,263 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Marlon
+ */
+public class auth {
+    public String leerArchivo(String ruta) {
+      File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+ 
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+        if(Static.isWindows()){            
+         archivo = new File (System.getProperty("user.home")+"\\Documents\\Altel\\Files\\"+ruta);
+        }else archivo = new File (System.getProperty("user.home")+"/Documents/Altel/Files/"+ruta);
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+         while((linea=br.readLine())!=null)
+            return linea;
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+      return "0"; 
+   }
+    
+public void escribeFichero(String linea,String nombre) throws IOException
+    {
+        verificaCarpetas();
+        String ruta = System.getProperty("user.home")+"/Documents/Altel/Files/"+nombre;
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(verificaFichero(archivo)) {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(linea);
+        } else {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(linea);
+        }
+        bw.close();
+    }
+
+    private String getFecha() {
+        Calendar cal = Calendar.getInstance();
+        return addCero(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)),2) +"/"+addCero(Integer.toString(cal.get(Calendar.MONTH)+1),2) +"/"+ Integer.toString(cal.get(Calendar.YEAR));
+    }
+    
+    public String getDia() {
+        Calendar cal = Calendar.getInstance();
+        return addCero(Integer.toString(cal.get(Calendar.DAY_OF_MONTH)),2);
+    }
+
+    public String getYear() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        return addCero(Integer.toString(cal.get(Calendar.YEAR)),2);
+    }
+
+    public String getMes() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        return addCero(Integer.toString(cal.get(Calendar.MONTH)+1),2);
+    }
+    public String get2MesAntes() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        if("01".equals(getMes())){return "11";}
+        if("02".equals(getMes())){return "12";}
+        return addCero(Integer.toString(cal.get(Calendar.MONTH)),2);
+    }
+    public String getMesAntes() {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        if("01".equals(getMes())){return "12";}
+        return addCero(Integer.toString(cal.get(Calendar.MONTH)),2);
+    }
+     private String getHora() {
+        Calendar cal = Calendar.getInstance();
+        return addCero(Integer.toString(cal.get(Calendar.HOUR_OF_DAY)),2) +":"+ addCero(Integer.toString(cal.get(Calendar.MINUTE)),2) +":"+ addCero(Integer.toString(cal.get(Calendar.SECOND)),2);
+    }
+public String[] bitacora(){
+    File Bitacora;
+        if(Static.isWindows()){
+            Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+        }else Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+    return Bitacora.list();
+}    
+public String returnRow(String fileName){
+        String reporte="";
+        BufferedReader br;
+        try {
+            if(Static.isWindows())br = new BufferedReader(new FileReader(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+fileName));
+            else br = new BufferedReader(new FileReader(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+fileName));
+        String line;
+        int cuenta=0;
+        try {
+        while ((line = br.readLine()) != null) {
+            reporte+=line+"&-&";
+           cuenta++;
+        }
+        br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TextPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return reporte;
+    }
+public void escribeFicheroPrint(String monto,String num,String empresa,String nombre) throws IOException
+    {
+        verificaCarpetas();
+        String fecha;
+        File Bitacora;
+        if(Static.isWindows()){
+            fecha= leerArchivo("Bitacora\\archivoFecha.txt"); 
+            Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+        }  
+        else {
+            fecha= leerArchivo("Bitacora/archivoFecha.txt");
+            Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+        } 
+        if(!fecha.equals(getMes())){
+            String[] listArchivos=Bitacora.list();
+            int total=0;
+            if(listArchivos.length>0){
+                while(!listArchivos[total].split("-")[1].equals(get2MesAntes())&&!fecha.equals(getMes())&&Bitacora.list().length>11){
+                    File removed;
+                    if(Static.isWindows()){
+                        Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");                    
+                        removed = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+listArchivos[total]);
+                    }else{
+                        Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");                    
+                        removed = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+listArchivos[total]);
+                    }
+                    
+                    removed.delete();
+                    total++;
+                }
+            }
+           /* if(total>11){
+                for(int i=0;i<total-11;i++){  
+                    int j=i+1;
+                    File removed = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora\\"+listArchivos[i]);
+                    removed.delete();
+                }
+            }*/
+            escribeFichero(getMes(), "Bitacora/archivoFecha.txt");
+            escribeFichero("0", "Bitacora/archivoTransacciones.txt");
+        }
+        String trans;
+       if(Static.isWindows()){                        
+            trans= leerArchivo("Bitacora\\archivoTransacciones.txt");
+       }else trans= leerArchivo("Bitacora/archivoTransacciones.txt");
+            int cuantas=Integer.parseInt(trans)+1;
+            escribeFichero(addCero(cuantas+"",2)+"", "Bitacora/archivoTransacciones.txt");
+            
+        File fout = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora/"+getYear()+"-"+getMes()+"-"+getDia()+"-"+addCero(cuantas+"",2)+".txt");
+      
+	FileOutputStream fos = new FileOutputStream(fout);
+ 
+	OutputStreamWriter osw = new OutputStreamWriter(fos);
+            
+                    if(Static.isWindows())osw.write(new auth().leerArchivo("Sesion\\archivoUser.txt"));
+                    else osw.write(new auth().leerArchivo("Sesion/archivoUser.txt"));
+                    osw.write(System.lineSeparator());
+                    osw.write(getFecha());
+                    osw.write(System.lineSeparator());
+                    osw.write(getHora());
+                    osw.write(System.lineSeparator());
+                    osw.write(nombre);
+                    osw.write(System.lineSeparator());
+                    osw.write(num);
+                    osw.write(System.lineSeparator());
+                    osw.write(empresa);
+                    osw.write(System.lineSeparator());
+                    osw.write(monto);
+ 
+	osw.close();
+    }
+
+public Boolean firstLogin()
+    {
+        File Sesion;
+            File Bitacora;
+        if(Static.isWindows()){
+            Sesion = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Sesion");
+            Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+        }else{
+            Sesion = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Sesion");
+            Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+        }
+        //File folderSesion = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Sesion");
+        String ruta = System.getProperty("user.home")+"/Documents/Altel/Files/Sesion/archivoPassword.txt";        
+        File archivo = new File(ruta);
+        if (!archivo.exists()) {
+            Sesion.mkdirs();
+            Bitacora.mkdirs();
+            try {
+                escribeFichero("true","Sesion/archivoPidePassword.txt");
+            } catch (IOException ex) {
+                Logger.getLogger(auth.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        }
+        return true;
+    }
+
+public void verificaCarpetas(){ 
+       // return file.exists();
+            File Sesion;
+            File Bitacora;
+        if(Static.isWindows()){
+            Sesion = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Sesion");
+            Bitacora = new File(System.getProperty("user.home")+"\\Documents\\Altel\\Files\\Bitacora");
+        }else{
+            Sesion = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Sesion");
+            Bitacora = new File(System.getProperty("user.home")+"/Documents/Altel/Files/Bitacora");
+        }
+        
+        if (!Sesion.exists()) {
+            Sesion.mkdirs();
+        }
+        if (!Bitacora.exists()) {
+            Bitacora.mkdirs();
+        }
+}
+public Boolean verificaFichero(File file){ 
+        return file.exists();
+}
+
+public String addCero(String monto, int i) {
+        return String.format("%0"+i+"d", Integer.parseInt(monto));
+    };
+}
+
